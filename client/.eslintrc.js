c829f29ad9589d075c1d9ceec3ff565ffb5179ce
Mehmet Bajin <10424048+mehmetbajin@users.noreply.github.com>
2021-05-31 18:15:40 -0400

    build(lint/format): update CI checks

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     node: true,
   },
   extends: [
+    '../.eslintrc.js',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
