69aafcdc33968f49bdab15287627af46daf18281
Mehmet Bajin <10424048+mehmetbajin@users.noreply.github.com>
2021-06-22 22:04:00 -0400

    fix(postgres): local vs. remote config

--- server/src/config/postgres.ts
+++ server/src/config/postgres.ts
@@ -1,16 +1,26 @@
-export interface PostgresConfig {
-  connection: string;
+import { PgConnectionConfig } from 'knex';
+
+export interface PostgresConfig extends PgConnectionConfig {
+  connectionString: string;
 }
 
 export const config: PostgresConfig = {
-  connection:
+  connectionString:
     process.env.OMSCENTRAL_POSTGRES_CONNECTION ||
     process.env.DATABASE_URL ||
     '',
 };
 
-if (!config.connection) {
+if (!config.connectionString) {
   throw new Error(
     'process.env.OMSCENTRAL_POSTGRES_CONNECTION || process.env.DATABASE_URL required',
   );
+}
+
+if (config.connectionString.includes('@localhost:')) {
+  config.ssl = false;
+} else {
+  config.ssl = {
+    rejectUnauthorized: false,
+  };
 }
