7dcb3948efa6878d44b85eb1bb022a72b8e92bde
Mehmet Bajin <mehmet@bajinlabs.com>
2021-08-21 13:12:25 -0400

    feat(learn-more): new direction

--- client/src/components/LearnMore/LearnMore.styles.ts
+++ client/src/components/LearnMore/LearnMore.styles.ts
@@ -7,4 +7,22 @@ export const useStyles = makeStyles((theme) => ({
   link: {
     fontWeight: 'bold',
   },
+  avatar: {
+    margin: theme.spacing(2, 'auto', 4),
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  action: {
+    width: '100%',
+  },
+  centered: {
+    textAlign: 'center',
+  },
+  message: {
+    margin: theme.spacing(2, 0),
+  },
 }));
