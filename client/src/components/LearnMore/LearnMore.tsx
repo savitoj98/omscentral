f80063b4e6c7a352cca250f74fddb1a9861fe024
Mehmet Bajin <mehmet@bajinlabs.com>
2021-10-02 16:42:08 -0400

    feat(client): LearnMore is back

--- client/src/components/LearnMore/LearnMore.tsx
+++ client/src/components/LearnMore/LearnMore.tsx
@@ -1,4 +1,4 @@
-import Avatar from '@material-ui/core/Avatar';
+// import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Dialog from '@material-ui/core/Dialog';
@@ -19,7 +19,7 @@ import { AuthContext } from '../Auth';
 import Link from '../Link';
 import { NotificationContext } from '../Notification';
 import { useStyles } from './LearnMore.styles';
-import avatarImageUri from './mehmet.jpg';
+// import avatarImageUri from './mehmet.jpg';
 
 const LearnMore: React.FC = () => {
   const classes = useStyles();
@@ -29,7 +29,7 @@ const LearnMore: React.FC = () => {
   const [learnMore, { loading }] = useLearnMoreMutation();
 
   const [open, setOpen] = useState(false);
-  const [submitted, setSubmitted] = useLocal<boolean>('/c:coaching', false);
+  const [submitted, setSubmitted] = useLocal<boolean>('/c:e3e4', false);
   const [message, setMessage] = useState('');
 
   const handleOpen = () => {
@@ -51,9 +51,7 @@ const LearnMore: React.FC = () => {
     if (result.data?.learnMore.success) {
       setOpen(false);
       setSubmitted(true);
-      notification?.success(
-        "Thank you for your interest. I'll be in touch within 24 hours!",
-      );
+      notification?.success("Thank you. I'll be in touch soon!");
     } else {
       notification?.error('Something went wrong. Please try again.');
     }
@@ -66,10 +64,9 @@ const LearnMore: React.FC = () => {
   return (
     <Container component="main" maxWidth="xl" className={classes.container}>
       <Alert severity="info">
-        <AlertTitle>Looking for a career in FAANG?</AlertTitle>
-        I&apos;m Mehmet (the creator of omscentral.com) and I&apos;ve worked at
-        and landed job offers from Facebook, Amazon, Apple, Netflix, and Google.
-        I think we can do great things together.{' '}
+        <AlertTitle>Facebook is hiring!</AlertTitle>
+        I&apos;m <Link to="https://github.com/mehmetbajin">Mehmet</Link> (the
+        creator of omscentral.com) and my team is hiring at Facebook.{' '}
         <Link to="#" onClick={handleOpen} className={classes.link}>
           Learn more →
         </Link>
@@ -81,34 +78,27 @@ const LearnMore: React.FC = () => {
         aria-labelledby="learn-more-dialog-title"
       >
         <DialogTitle id="learn-more-dialog-title" className={classes.centered}>
-          Hi there, I&apos;m Mehmet! 😄
+          Ready to solve privacy for the world? 🌎
         </DialogTitle>
         <DialogContent>
-          <Avatar
+          {/* <Avatar
             alt="Mehmet Bajin"
             src={avatarImageUri}
             className={classes.avatar}
-          />
+          /> */}
           <DialogContentText>
-            Thank you for being here. Your support means the world to me. 🙏
+            Facebook is hiring{' '}
+            <Link to="https://www.levels.fyi/company/Facebook/salaries/">
+              E3 and E4
+            </Link>{' '}
+            software engineers to work on privacy. Prefer US/Eastern time zone
+            candidates. 0-4 years of experience. Remote OK.
             <br />
             <br />
-            As a software engineer in FAANG, I love helping others find joy and
-            fulfillment in their careers. After graduating from Georgia Tech in
-            2009, I went to Emory medical school only to drop out during my
-            first year. Now, I help solve privacy for the world at Facebook.
-            Along the way, I&apos;ve worked at and landed job offers from every
-            FAANG company, and I know what it takes to be successful in the tech
-            industry. 😎
+            Interested?
             <br />
             <br />
-            I&apos;d love to get to know you better and discover the great
-            things we can accomplish together, whether it&apos;s a career in big
-            tech, freelancing at a multi-national, or starting your own
-            business. 🥳
-            <br />
-            <br />
-            Once you shoot me a message, I&apos;ll be in touch with next
+            Then shoot me a message, and I&apos;ll be in touch with next
             steps&mdash;looking forward to hearing from you! 🙇‍♂️
           </DialogContentText>
           <TextField
