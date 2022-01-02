4ef0ac22026fe1bddaed734a678d02d96e2038c9
Mehmet Bajin <10424048+mehmetbajin@users.noreply.github.com>
2022-01-02 15:50:25 -0500

    style(client): more spacing around <Paper />

--- client/src/components/Paper/Paper.styles.ts
+++ client/src/components/Paper/Paper.styles.ts
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme) => ({
   paper: {
-    margin: theme.spacing(2, 0),
+    margin: theme.spacing(4, 0),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
