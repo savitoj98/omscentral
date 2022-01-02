b526ac62cedac1bf1a448c17512abbb7db523242
Mehmet Bajin <mehmet@bajinlabs.com>
2022-01-01 19:53:35 -0500

    build(deps): bump

--- client/src/components/Link/Link.tsx
+++ client/src/components/Link/Link.tsx
@@ -6,34 +6,35 @@ import { Link as RouterLink, LinkProps } from 'react-router-dom';
 
 import { FirebaseContext } from '../Firebase';
 
-const Link: React.FC<LinkProps & { to: string | null; onClick?: () => void }> =
-  (props) => {
-    const history = useHistory();
-    const firebase = useContext(FirebaseContext);
+const Link: React.FC<
+  LinkProps & { to: string | null; onClick?: () => void }
+> = (props) => {
+  const history = useHistory();
+  const firebase = useContext(FirebaseContext);
 
-    const handleClick = (event: React.MouseEvent) => {
+  const handleClick = (event: React.MouseEvent) => {
+    event.preventDefault();
+    const { to, onClick } = props;
+    if (onClick) {
       event.preventDefault();
-      const { to, onClick } = props;
-      if (onClick) {
-        event.preventDefault();
-        onClick();
-      } else if (to === '') {
-        history.goBack();
-      } else if (/^http(s?):\/\//.test(to)) {
-        logEvent(firebase.analytics, 'screen_view', {
-          app_name: 'external_link',
-          firebase_screen: to,
-          firebase_screen_class: 'Link',
-        });
-        window.open(to); // eslint-disable-line
-      } else {
-        history.push(to);
-      }
-    };
-
-    return (
-      <RouterLink component={MaterialLink} {...props} onClick={handleClick} />
-    );
+      onClick();
+    } else if (to === '') {
+      history.goBack();
+    } else if (/^http(s?):\/\//.test(to)) {
+      logEvent(firebase.analytics, 'screen_view', {
+        app_name: 'external_link',
+        firebase_screen: to,
+        firebase_screen_class: 'Link',
+      });
+      window.open(to); // eslint-disable-line
+    } else {
+      history.push(to);
+    }
   };
+
+  return (
+    <RouterLink component={MaterialLink} {...props} onClick={handleClick} />
+  );
+};
 
 export default Link;
